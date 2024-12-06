006b94799f3f327772216444bb1dd20e18de8e7e
Carolina Neves <163293121+carolinasneves@users.noreply.github.com>
2024-12-06 17:18:45 +0000

    putting we store

---
+++ data/WE STORE/store tilesets/STORE TILESET1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="STORE TILESET1" tilewidth="16" tileheight="16" tilecount="792" columns="33">
+ <image source="CosyCabin_TileMap_Simple.png" width="528" height="384"/>
+</tileset>
