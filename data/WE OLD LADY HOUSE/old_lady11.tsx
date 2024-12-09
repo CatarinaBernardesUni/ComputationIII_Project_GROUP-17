f23ad98c2995a32bb3938c3076135db224d1aef3
Carolina Alves Ferreira <20231719@novaims.unl.pt>
2024-12-09 01:02:34 +0000

    old lady

--- data/WE OLD LADY HOUSE/old_lady11.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="old_lady" tilewidth="16" tileheight="16" tilecount="2538" columns="54">
- <image source="../../../../../../Downloads/Texto do seu parágrafo (1).png" width="879" height="753"/>
-</tileset>
