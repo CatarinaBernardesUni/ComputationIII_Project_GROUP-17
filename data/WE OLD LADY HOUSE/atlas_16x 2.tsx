19d9eca2a0915f5834c5d1aef521182e05900b31
CatarinaBernardesUni <20231609@novaims.unl.pt>
2024-12-09 01:21:17 +0000

    tiny fixes

--- data/WE OLD LADY HOUSE/atlas_16x 2.tsx
+++ data/WE OLD LADY HOUSE/atlas_16x 2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="atlas_16x 2" tilewidth="16" tileheight="16" tilecount="768" columns="48">
- <image source="../../../../tilesets or whatever/Super_Retro_World_Interior_pack_week42/atlas_16x.png" width="768" height="256"/>
+ <image source="old_lady_house tilesets/atlas_16x.png" width="768" height="256"/>
 </tileset>
--- data/WE OLD LADY HOUSE/old_lady13.tsx
+++ data/WE OLD LADY HOUSE/old_lady13.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="old_lady13" tilewidth="16" tileheight="16" tilecount="30" columns="6">
- <image source="../../../../../../Downloads/Texto_do_seu_parágrafo__2_-removebg-preview (2).png" width="100" height="86"/>
+<tileset version="1.10" tiledversion="1.11.0" name="old_lady13" tilewidth="16" tileheight="16" tilecount="3762" columns="66">
+ <image source="old_lady_house tilesets/CosyCabin_Objects_Simple.png" width="1056" height="912"/>
 </tileset>
