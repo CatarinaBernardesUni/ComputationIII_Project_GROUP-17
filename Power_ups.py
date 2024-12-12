39e1e190b9145ff114ab8fe00d82ee6f67b02ed1
Carolina Alves Ferreira <20231719@novaims.unl.pt>
2024-12-12 12:37:20 +0000

    health e power up class only added

---
+++ Power_ups.py
@@ -0,0 +1,12 @@
+from config import *
+
+
+class Power_ups(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+    def affect_player(self):
+        pass
+
+    def affect_game(self):
+        pass
