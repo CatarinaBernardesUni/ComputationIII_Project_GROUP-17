57563159890aabde27d7f1fcbe33f6bed9ed9a96
CatarinaBernardesUni <20231609@novaims.unl.pt>
2024-12-21 22:13:37 +0000

    plants in greenhouse stop being animated when they are fully grown

--- tile.py
+++ tile.py
@@ -10,13 +10,16 @@ class Tile(pygame.sprite.Sprite):
         self.animation_duration = animation_duration if animation_duration else 1
         self.animation_time = 0  # Store time passed for animation
 
-    def update(self, time_frame):
+    def update(self, time_frame, loop=True):
         if self.animation_frames:
             self.animation_time += time_frame
             if self.animation_time >= self.animation_duration:
-                self.current_anim_index += 1
-                if self.current_anim_index >= len(self.animation_frames):
-                    self.current_anim_index = 0
-                self.image = self.image = self.animation_frames[self.current_anim_index]
                 self.animation_time = 0
+                if self.current_anim_index < len(self.animation_frames) - 1:
+                    # Move to the next frame if not at the last frame
+                    self.current_anim_index += 1
+                elif loop:
+                    # If we want it to loop, return to the first frame
+                    self.current_anim_index = 0
+                self.image = self.animation_frames[self.current_anim_index]
 
